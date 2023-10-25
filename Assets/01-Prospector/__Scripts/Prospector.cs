a73180a7a678e2cebc2cccb13b4d66e0cf18a1a4
Matthew Sissick <120825879+Majorhavoc123@users.noreply.github.com>
2023-10-24 20:01:36 -0400

    This is where the error is occuring
    Prospector.cs:76
    
    For whatever reason, if this line is not commented out Unity gives me the error message: "Object reference not set to an instance of an object"
    
    When I comment this line out, the game works ass intended but it breaks the scoring and that won't work, even though the cards are loaded.

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -73,7 +73,7 @@ public class Prospector : MonoBehaviour
 
     void Start()
     {
-        Scoreboard.S.score = ScoreManager.SCORE;
+        // Scoreboard.S.score = ScoreManager.SCORE;
         deck = GetComponent<Deck>();
         deck.InitDeck(deckXML.text);
         Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference
